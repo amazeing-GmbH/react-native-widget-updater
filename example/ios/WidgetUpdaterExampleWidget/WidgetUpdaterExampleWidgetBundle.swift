f77b065e9a10d6bbde9d3a98de0f050629ee15ee
Martin Mazein <internet+github@amazeing.net>
2024-02-03 19:13:51 +0100

    feat(example-app,ios): add functional example app with widget on iOS
    Signed-off-by: Martin Mazein <internet+github@amazeing.net>

---
+++ example/ios/WidgetUpdaterExampleWidget/WidgetUpdaterExampleWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetUpdaterExampleWidgetBundle.swift
+//  WidgetUpdaterExampleWidget
+//
+//  Created by Martin Mazein on 2024-02-03.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetUpdaterExampleWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetUpdaterExampleWidget()
+    }
+}
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,53 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-widget-updater';
+import { StyleSheet, View, Text, Button, Platform } from 'react-native';
+import {
+  type AppWidgetIds,
+  getAppWidgetIds,
+  updateAppWidgets,
+} from 'react-native-widget-updater';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState<AppWidgetIds>({});
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    const widgetClasses: string[] = Platform.select({
+      android: ['.ExampleAppWidget'],
+      default: ['WidgetUpdaterExampleWidget'],
+    });
+    getAppWidgetIds(widgetClasses).then((widgetIds: AppWidgetIds) =>
+      setResult(widgetIds)
+    );
   }, []);
+
+  const renderResult = (): React.ReactNode => {
+    const results: string[] = [];
+
+    if (Object.keys(result).length === 0) {
+      results.push(
+        'Please add an example widget to the home screen and re-open the app.'
+      );
+    } else {
+      Object.keys(result).forEach((r) => {
+        results.push(`${r} => ${result[r]}`);
+      });
+    }
+
+    return results.join('\n');
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.text}>Result: {renderResult()}</Text>
+      <Button
+        title="Update widgets"
+        disabled={Object.keys(result).length === 0}
+        onPress={() => {
+          if (Object.keys(result).length !== 0) {
+            updateAppWidgets(Object.keys(result));
+          }
+        }}
+      />
     </View>
   );
 }
@@ -22,10 +57,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 32,
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  text: {
+    paddingBottom: 20,
   },
 });
